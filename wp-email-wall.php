e02713708ab71ddcf3a95c55c68e3338e24e73cd
Koji Flowers <kojiflowers@gmail.com>
2018-07-27 17:13:37 -0400

    initial commit + added .gitignore + added main plugin file

---
+++ wp-email-wall.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:  WP Email Wall Plugin
+Plugin URI:   https://kojiflowers.com
+Description:  A Wordpress plugin that prevents blocked email domains from registering as a user and removes any existing users with blocked email domains
+Version:      1.0
+Author:       Koji Flowers
+Author URI:   https://kojiflowers.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
